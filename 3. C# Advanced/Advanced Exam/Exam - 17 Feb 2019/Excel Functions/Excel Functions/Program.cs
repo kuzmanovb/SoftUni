8a30bb7b9f8bf030a00fea49dd447f81750b7572
Boian Kuzmanov <kuzmanovb@abv.bg>
2020-01-24 15:26:58 +0200

    Add Advanced Exam Task

---
+++ 3. C# Advanced/Advanced Exam/Exam - 17 Feb 2019/Excel Functions/Excel Functions/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Excel_Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberInput = int.Parse(Console.ReadLine());
+
+            var headerInputData = new List<string>();
+            var containInputData = new List<List<string>>();
+            for (int i = 0; i < numberInput; i++)
+            {
+                var input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).ToList();
+                if (i == 0)
+                {
+                    headerInputData = input;
+                }
+                else
+                {
+                    containInputData.Add(input);
+                }
+            }
+
+            var command = Console.ReadLine().Split();
+
+            if (command[0] == "hide")
+            {
+                HideHeader(containInputData, headerInputData, command);
+            }
+            else if (command[0] == "sort")
+            {
+                containInputData = SortByHeader(containInputData, headerInputData, command);
+            }
+            else if (command[0] == "filter")
+            {
+                FilterByHeader(containInputData, headerInputData, command);
+            }
+
+            PrintContainInputData(containInputData, headerInputData);
+
+        }
+
+        private static void PrintContainInputData(List<List<string>> containInputData, List<string> header)
+        {
+            Console.WriteLine(string.Join(" | ", header));
+            foreach (var item in containInputData)
+            {
+                Console.WriteLine(string.Join(" | ", item));
+            }
+        }
+
+        private static void FilterByHeader(List<List<string>> containInputData, List<string> header, string[] command)
+        {
+            var curentHeader = command[1];
+            var value = command[2];
+            var indexColumb = FindIndexColumb(header, curentHeader); ;
+
+            for (int i = 0; i < containInputData.Count; i++)
+            {
+                if (!containInputData[i][indexColumb].Contains(value))
+                {
+                    containInputData.RemoveAt(i);
+                    i -= 1;
+                }
+            }
+        }
+
+        private static List<List<string>> SortByHeader(List<List<string>> containInputData, List<string> header, string[] command)
+        {
+            var curentHeader = command[1];
+            var indexColumb = FindIndexColumb(header, curentHeader);
+
+            if (indexColumb >= 0)
+            {
+                containInputData = containInputData.OrderBy(x => x[indexColumb]).ToList();
+            }
+            return containInputData;
+        }
+
+        private static void HideHeader(List<List<string>> containInputData, List<string> header, string[] command)
+        {
+            var curentHeader = command[1];
+            var indexColumb = FindIndexColumb(header, curentHeader);
+            header.RemoveAt(indexColumb);
+            foreach (var item in containInputData)
+            {
+                item.RemoveAt(indexColumb);
+            }
+        }
+
+        private static int FindIndexColumb(List<string> header, string curentHeader)
+        {
+            var index = -1;
+            for (int i = 0; i < header.Count; i++)
+            {
+                if (header[i] == curentHeader)
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            return index;
+        }
+    }
+}
---
+++ 3. C# Advanced/Advanced Exam/Exam - 17 Feb 2019/Socks/Socks/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Socks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var leftSocksInput = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var rightSocksInput = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var leftSocks = new Stack<int>(leftSocksInput);
+            var rightSocks = new Queue<int>(rightSocksInput);
+
+            var pairSocks = new List<int>();
+
+            while (leftSocks.Any() && rightSocks.Any())
+            {
+                var curentLeftSock = leftSocks.Peek();
+                var curentRightSock = rightSocks.Peek();
+
+                if (curentLeftSock > curentRightSock)
+                {
+                    pairSocks.Add(leftSocks.Pop() + rightSocks.Dequeue());
+                }
+                else if (curentLeftSock < curentRightSock)
+                {
+                    leftSocks.Pop();
+                }
+                if (curentLeftSock == curentRightSock)
+                {
+                    rightSocks.Dequeue();
+                    leftSocks.Push(leftSocks.Pop() + 1);
+                }
+
+            }
+
+            Console.WriteLine(pairSocks.Max());
+            Console.WriteLine(string.Join(" ", pairSocks));
+        }
+    }
+}
